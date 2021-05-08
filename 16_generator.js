80ad2d145b283d0c85be7bc379e3b6772c4450de
usernaimandrey <anshlyapnikov@yandex.ru>
2021-05-08 08:30:32 +0300

    add generator.js

---
+++ 16_generator.js
@@ -0,0 +1,20 @@
+import { mkdir, mkfile } from '@hexlet/immutable-fs-trees';
+
+const generator = () => {
+  const tree = mkdir('nodejs-package',
+    [mkfile('Makefile'),
+      mkfile('README.md'),
+      mkdir('dist'),
+      mkdir('__tests__', [mkfile('half.test.js', { type: 'text/javascript' })]),
+      mkfile('babel.config.js', { type: 'text/javascript' }),
+      mkdir('node_modules', [
+        mkdir('@babel', [
+          mkdir('cli', [
+            mkfile('LICENSE'),
+          ]),
+        ]),
+      ], { owner: 'root', hidden: false })], { hidden: true });
+  return tree;
+};
+
+export default generator;
